2947aa1c8aac1ac6ba4e2890b4b395a3411a6345
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2021-11-23 13:53:49 +0200

    Fix null safety issue

--- lib/src/collection.dart
+++ lib/src/collection.dart
@@ -103,9 +103,11 @@ class GeoFireCollectionRef {
     var filtered = mergedObservable.map((List<DistanceDocSnapshot> list) {
       var mappedList = list.map((DistanceDocSnapshot distanceDocSnapshot) {
         // split and fetch geoPoint from the nested Map
-        final fieldList = field.split('.');
-        var geoPointField =
-            distanceDocSnapshot.documentSnapshot.data()[fieldList[0]];
+        final List<String> fieldList = field.split('.');
+        // var geoPointField =
+        //     distanceDocSnapshot.documentSnapshot.data()[fieldList[0]];
+        final Map data = distanceDocSnapshot.documentSnapshot.data();
+        var geoPointField = data[fieldList[0]];
         if (fieldList.length > 1) {
           for (int i = 1; i < fieldList.length; i++) {
             geoPointField = geoPointField[fieldList[i]];
